4b192d9bcfa04243988683fa0bb45a25e370c5fd
Maxi <maxisaias2009@gmail.com>
2023-09-21 08:42:38 -0300

    se crea la app2 con sus vists y el archivo .gitignore

---
+++ mihg_app2/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MihgApp2Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mihg_app2'
--- mihg_proyecto1/urls.py
+++ mihg_proyecto1/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mihg_app2 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pagina1/', views.vista),
+    path('pagina2/', views.vista2)
 ]
